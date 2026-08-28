2fc30dec7a79d004b5eaf6b7e3201e58a6ea47a0
xdliw <xddliw@gmail.com>
2026-08-28 15:48:57 +0300

    deleted contests folder

---
+++ solving/J/J.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#ifdef LOCAL
+#include "my/writings/debug.h"
+#else
+#define dbg(...) 0
+#define show(x) 0
+#endif
+
+
+void solve(){
+
+
+
+}
+
+int32_t main(){
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+	int32_t t; cin >> t;
+	while(t--){
+		solve();
+	}
+
+	return 0;
+}
